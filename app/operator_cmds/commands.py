eb8b9e08a0edb3064620ffe3cc2524f5929ecdd2
Meni Yakove <myakove@gmail.com>
2023-03-16 10:09:29 +0200

    raise NotImplementedError for operator

--- app/operator_cmds/commands.py
+++ app/operator_cmds/commands.py
@@ -8,17 +8,18 @@ def operator(ctx):
     Command line to Install/Uninstall Operator on OCP cluster.
     """
     ctx.ensure_object(dict)
+    raise NotImplementedError
 
 
 @operator.command()
 @click.pass_context
 def install(ctx):
     """Install cluster Operator."""
-    print(ctx)
+    raise NotImplementedError
 
 
 @operator.command()
 @click.pass_context
 def uninstall(ctx):
     """Uninstall cluster Operator."""
-    print(ctx)
+    raise NotImplementedError
